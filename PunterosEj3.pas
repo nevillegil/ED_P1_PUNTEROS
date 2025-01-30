a9bd235a8bc29c33b54514624d1e370ad8cfe723
Neville <nevillegilor@gmail.com>
2025-01-30 18:26:36 +0100

    Update PunterosEj3.pas

--- PunterosEj3.pas
+++ PunterosEj3.pas
@@ -11,23 +11,23 @@ var
         varnodo: ^nodo;
 begin
     { Crea un nodo en memoria dinámica (heap) }
-
+        new(varnodo);
 
     { Dale valor 100 al campo entero de este nodo }
-
+        nodo.entero:= 100;
 
     { Pon a nil el campo puntero del nodo }
-
+        varnodo.puntero:=nil;
 
     { Pon ahora el puntero del nodo a apuntar al propio nodo }
-
+        varnodo.puntero^:=varnodo;
 
     { En este punto hay dos instrucciones alternativas para liberar la memoria dinámica creada: }
     { Alternativa 1: Liberar el nodo directamente }
-
+        dispose(varnodo);
 
     { Alternativa 2: Liberar el nodo a través del campo puntero }
     { Dispose(punteroNodo^.siguiente); }
-
+        dispose(varnodo^.puntero);
     { Termina }
 end.
