492f857017afc9142528401323396a71c9be3346
Neville <nevillegilor@gmail.com>
2025-02-02 19:26:32 +0100

    Update PunterosEj4.pas

--- PunterosEj4.pas
+++ PunterosEj4.pas
@@ -18,43 +18,85 @@ var
 { 6.a Implementa una función que calcule el máximo número del array (debes comprobar 
 antes, pues puede que alguna posición no tenga un número y esté a NIL)}
 function MaxArray(ptrArray: tparray): Integer;
+var
+    i: Integer;
+    maxValue: Integer;
 begin
-    // Implementar esta función
-    WriteLn('No implementado todavía');
+    maxValue := -MaxInt; // Inicializamos con el menor valor posible
+    for i := 0 to MAX-1 do
+    begin
+        if (ptrArray[i] <> NIL) and (ptrArray[i]^ > maxValue) then
+            maxValue := ptrArray[i]^;
+    end;
+    MaxArray := maxValue;
 end;
 
 { 7.a Implementa una función que compute la suma de todos los enteros (de nuevo 
 asegúrate de no intentar sumar los que estén a NIL) }
 function SumArray(ptrArray: tparray): Integer;
+var
+    i, sum: Integer;
 begin
-    // Implementar esta función
-    WriteLn('No implementado todavía');
+    sum := 0;
+    for i := 0 to MAX-1 do
+    begin
+        if ptrArray[i] <> NIL then
+            sum := sum + ptrArray[i]^;
+    end;
+    SumArray := sum;
 end;
+
 
 { 3.a Implementa un procedimiento que muestre por pantalla el contenido de cada posición }
 procedure PrintArray(ptrArray: tparray);
+var
+    i: Integer;
 begin
-    // Implementar este procedimiento
-    WriteLn('No implementado todavía');
+    for i := 0 to MAX-1 do
+    begin
+        if ptrArray[i] <> NIL then
+            WriteLn('Posición ', i, ': ', ptrArray[i]^)
+        else
+            WriteLn('Posición ', i, ': NIL');
+    end;
 end;
 
 { 4.a Implementa un procedimiento que ponga a cero todos los números }
 procedure ZeroArray(var ptrArray: tparray);
+var
+    i: Integer;
 begin
-    // Implementar este procedimiento
-    WriteLn('No implementado todavía');
+    for i := 0 to MAX-1 do
+    begin
+        if ptrArray[i] <> NIL then
+            ptrArray[i]^ := 0;
+    end;
 end;
 
 { 5. Implementa un subprograma que ponga todos los punteros que no lo estén, a NIL }
 procedure NilArray(ptrArray: tparray);
+var
+    i: Integer;
 begin
-    // Implementar este procedimiento
-    WriteLn('No implementado todavía');
+    for i := 0 to MAX-1 do
+    begin
+        if ptrArray[i] <> NIL then
+        begin
+            Dispose(ptrArray[i]); // Liberar memoria
+            ptrArray[i] := NIL;
+        end;
+    end;
 end;
 
 begin
     { 2. Crea un entero en cada una de las posiciones inicializado a un número aleatorio }
     // Implementar esta parte
+       Randomize;
+    for var i := 0 to MAX-1 do
+    begin
+        New(ptrArray[i]); // Reservar memoria
+        ptrArray[i]^ := Random(100); // Asignar valor aleatorio
+    end;
 
     repeat
         ClrScr;
